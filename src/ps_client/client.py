fb69f3868517f44699c1646ab366bbedf52807ba
Krishnan M <Krishnan.Mani@ashmus.ox.ac.uk>
2026-03-05 09:15:19 +0000

    support specifying env and service

--- src/ps_client/client.py
+++ src/ps_client/client.py
@@ -2,35 +2,41 @@ import boto3
 
 
 class ConfigClient:
-
     def __init__(self, environment: str, service: str, region: str):
         self.environment = environment
         self.service = service
-        self.client = boto3.client('ssm', region_name=region)
+        self.client = boto3.client("ssm", region_name=region)
 
-    def get(self, key: str, environment: str | None = None, service: str | None = None) -> str:
+    def get(
+        self, key: str, environment: str | None = None, service: str | None = None
+    ) -> str:
         return self._get_parameter(key, environment, service)
 
-    def get_secret(self, key: str, environment: str | None = None, service: str | None = None) -> str:
+    def get_secret(
+        self, key: str, environment: str | None = None, service: str | None = None
+    ) -> str:
         return self._get_parameter(key, environment, service, decrypt=True)
 
-    def _get_parameter(self, key: str, environment: str | None = None, service: str | None = None, decrypt: bool = False) -> str:
+    def _get_parameter(
+        self,
+        key: str,
+        environment: str | None = None,
+        service: str | None = None,
+        decrypt: bool = False,
+    ) -> str:
         _environment = environment or self.environment
         _service = service or self.service
         _key = get_key(key, _environment, _service)
         try:
-            response = self.client.get_parameter(
-                Name=_key,
-                WithDecryption=decrypt
-            )
-            parameter_type = response['Parameter']['Type']
-            if not decrypt and parameter_type == 'SecureString':
+            response = self.client.get_parameter(Name=_key, WithDecryption=decrypt)
+            parameter_type = response["Parameter"]["Type"]
+            if not decrypt and parameter_type == "SecureString":
                 raise ValueError(f"warning: [{key}] is encrypted.")
 
-            return response['Parameter']['Value']
+            return response["Parameter"]["Value"]
         except self.client.exceptions.ParameterNotFound as e:
             raise KeyError(e) from e
 
 
-def get_key(key: str, environment: str , service: str):
+def get_key(key: str, environment: str, service: str):
     return f"/{environment}/{service}/{key}"
--- tests/client_test.py
+++ tests/client_test.py
@@ -8,59 +8,59 @@ from ps_client import ConfigClient
 @mock_aws
 def test_get():
     # arrange
-    aws_client = boto3.client('ssm', region_name='us-east-1')
-    aws_client.put_parameter(Name='/test/service/foo', Value='bar', Type='String')
-    client = ConfigClient('test', 'service', 'us-east-1')
+    aws_client = boto3.client("ssm", region_name="us-east-1")
+    aws_client.put_parameter(Name="/test/service/foo", Value="bar", Type="String")
+    client = ConfigClient("test", "service", "us-east-1")
     # assert
-    assert client.get('foo') == 'bar'
+    assert client.get("foo") == "bar"
 
 
 @mock_aws
 def test_missing_key():
-    client = ConfigClient('test', 'service', 'us-east-1')
+    client = ConfigClient("test", "service", "us-east-1")
     with pytest.raises(KeyError, match=".*missing-key not found."):
-        client.get('missing-key')
+        client.get("missing-key")
 
 
 @mock_aws
 def test_wrong_region():
     # arrange
     virginia, sydney = "us-east-1", "ap-southeast-2"
-    aws_client = boto3.client('ssm', region_name=virginia)
-    aws_client.put_parameter(Name='/test/service/foo', Value='bar', Type='String')
-    client = ConfigClient('test', 'service', sydney) # region mis-configured
+    aws_client = boto3.client("ssm", region_name=virginia)
+    aws_client.put_parameter(Name="/test/service/foo", Value="bar", Type="String")
+    client = ConfigClient("test", "service", sydney)  # region mis-configured
     # assert
     assert virginia != sydney
     with pytest.raises(KeyError, match=".*foo not found."):
-        client.get('foo')
+        client.get("foo")
 
 
 @mock_aws
 def test_get_secret():
     # arrange
-    aws_client = boto3.client('ssm', region_name='us-east-1')
-    aws_client.put_parameter(Name='/test/service/foo', Value='bar', Type='SecureString')
-    client = ConfigClient('test', 'service', 'us-east-1')
+    aws_client = boto3.client("ssm", region_name="us-east-1")
+    aws_client.put_parameter(Name="/test/service/foo", Value="bar", Type="SecureString")
+    client = ConfigClient("test", "service", "us-east-1")
     # assert
-    assert client.get_secret('foo') == 'bar'
+    assert client.get_secret("foo") == "bar"
 
 
 @mock_aws
 def test_attempt_get_secret_for_clear_text_parameter():
     # arrange
-    aws_client = boto3.client('ssm', region_name='us-east-1')
-    aws_client.put_parameter(Name='/test/service/foo', Value='bar', Type='String')
-    client = ConfigClient('test', 'service', 'us-east-1')
+    aws_client = boto3.client("ssm", region_name="us-east-1")
+    aws_client.put_parameter(Name="/test/service/foo", Value="bar", Type="String")
+    client = ConfigClient("test", "service", "us-east-1")
     # assert
-    assert client.get_secret('foo') == 'bar'
+    assert client.get_secret("foo") == "bar"
 
 
 @mock_aws
 def test_attempt_get_for_cipher_text_parameter():
     # arrange
-    aws_client = boto3.client('ssm', region_name='us-east-1')
-    aws_client.put_parameter(Name='/test/service/foo', Value='bar', Type='SecureString')
-    client = ConfigClient('test', 'service', 'us-east-1')
+    aws_client = boto3.client("ssm", region_name="us-east-1")
+    aws_client.put_parameter(Name="/test/service/foo", Value="bar", Type="SecureString")
+    client = ConfigClient("test", "service", "us-east-1")
     # assert
     with pytest.raises(ValueError, match=".*foo] is encrypted."):
-        client.get('foo')
+        client.get("foo")
