b54055d856d70dd1f7cdb729ab2b3977f14dfbed
Krishnan M <Krishnan.Mani@ashmus.ox.ac.uk>
2026-03-09 12:11:59 +0000

    changing build tool, update deps

--- src/ps_client/__init__.py
+++ src/ps_client/__init__.py
@@ -1,8 +1,4 @@
-def hello() -> str:
-    return "Hello from ps-client!"
-
-
-from .client import ConfigClient
-
 __all__ = ["ConfigClient"]
-__version__ = "0.2.0"
+__version__ = "0.6.1"
+
+from ps_client.client import ConfigClient
